828912baeb19431f2d0702cc5153bd76a26c6494
Ujjwal Dikshit <ujjwaldikshit1@gmail.com>
2025-08-08 20:32:35 +0530

    added admin pannel

---
+++ day16/back/src/config/redis.js
@@ -0,0 +1,12 @@
+const { createClient }  = require('redis');
+
+const redisClient = createClient({
+    username: 'default',
+    password: process.env.REDIS_PASS,
+    socket: {
+        host: 'redis-14887.c264.ap-south-1-1.ec2.redns.redis-cloud.com',
+        port: 14887
+    }
+});
+module.exports = redisClient;
+
